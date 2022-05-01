7f40daf7c1760a2ad4913fbcb99b0423d295e986
Roberto Monplaisir <castedcrown84@gmail.com>
2022-05-01 13:59:03 -0400

    added gallery model

---
+++ models/Gallery.js
@@ -0,0 +1,71 @@
+const {Model, DataTypes} = require('sequelize')
+const sequelize = require('../config/connections')
+
+
+class Gallery extends Model {}
+
+Gallery.init(
+
+{
+
+    id: {
+
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+
+
+    },
+
+    name: {
+
+        type: DataTypes.STRING,
+        allowNull:false,
+
+
+    },
+
+    starting_date: {
+
+        type: DataTypes.DATE,
+        allowNull:false
+
+    },
+
+    ending_date: {
+
+        type:DataTypes.DATE,
+        allowNull:false
+
+
+    }
+
+
+
+
+
+
+
+
+},
+
+{
+
+    sequelize,
+    modelName: 'gallery',
+    freezeTableName: true,
+    underscored:true
+
+}
+
+
+
+
+
+
+
+
+
+
+)
