215cb401ae6d5d1e31a757f8f788fc09b84eedad
Roberto Monplaisir <castedcrown84@gmail.com>
2022-05-31 10:38:21 -0400

    required withauth function from  helpers folder

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require('path');
 const session = require('express-session');
 const routes = require('./controllers')
 const SequelizeStore = require('connect-session-sequelize')('session.Store')
+const helpers = require('./util/withauths')
 
 //for handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 
 
